ce4b4b689d97367137518ae66b346453bbaf33e5
Arthur Sun <arthursun2005@gmail.com>
2019-06-04 14:47:41 +1000

    Initial Commit

---
+++ SPH/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SPH
+//
+//  Created by Arthur Sun on 6/4/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
